2fee93f432edef79b9eae4a60af0306d35d05c32
Josué KANKU MWEMBO <jkanku@advanscongo.lan>
2025-04-21 15:22:43 +0100

    definition caisse model

--- app/Models/CaisseModel.php
+++ app/Models/CaisseModel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CaisseModel extends Model
 {
     use HasFactory;
+    protected $fillable = ['montant_total'];
+    public function client()
+    {
+        return $this->belongsTo(ClientModel::class);
+    }
 }
