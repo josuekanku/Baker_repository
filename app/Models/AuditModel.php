646180ed5484d553a6842f0275c62637b82873c0
Josué KANKU MWEMBO <jkanku@advanscongo.lan>
2025-04-21 15:21:30 +0100

    definition modele

--- app/Models/AuditModel.php
+++ app/Models/AuditModel.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class AuditModel extends Model
 {
     use HasFactory;
+    protected $fillable = ['vente_id', 'achat_id', 'montant', 'type', 'date'];
+
+    public function vente()
+    {
+        return $this->belongsTo(VenteModel::class);
+    }
+
+    public function achat()
+    {
+        return $this->belongsTo(AchatModel::class);
+    }
 }
